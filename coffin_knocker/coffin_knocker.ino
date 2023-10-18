77e52690ce72fbb8df45022856deef44fd46d9b6
Adrian McCarthy <github@aidtopia.com>
2023-10-18 15:48:17 -0700

    Reworked parameter handling in coffin knocker.

--- coffin_knocker/coffin_knocker.ino
+++ coffin_knocker/coffin_knocker.ino
@@ -36,110 +36,176 @@
 
 #include <EEPROM.h>
 
-int constexpr trigger_pin = 2;   // input, HIGH indicates motion
+int constexpr trigger_pin  = 2;  // input, HIGH indicates motion
 int constexpr solenoid_pin = 9;  // output, HIGH opens the valve
 
-// These parameters control the action.
-struct TimingParameters {
-  int version;
+class Parameters {
+  public:
+    // The suspense time is how long (in milliseconds) after motion is detected
+    // the action should begin.  A short delay after motion is detected may
+    // help in surprising victims, but this can be set to 0.
+    long suspense_time() const {
+      return get(SUSPENSE_TIME);
+    }
 
-  // VERSION 1
+    // After the suspense time, the coffin will knock randomly for the
+    // run time, which is selected randomly from a range.
+    long random_run_time() const {
+      return random(get(MIN_RUN_TIME), get(MAX_RUN_TIME));
+    }
 
-  // The suspense_time is how long (in milliseconds) after motion is detected
-  // the action should begin.  A short delay after motion is detected may help
-  // in surprising victims, but this can be set to 0.
-  long suspense_time;
+    // Each knock consists of a random on time...
+    long random_knock_on_time() const {
+      return random(get(MIN_KNOCK_ON_TIME), get(MAX_KNOCK_ON_TIME));
+    }
+    // ...followed by an off time.
+    long random_knock_off_time() const {
+      return random(get(MIN_KNOCK_OFF_TIME), get(MAX_KNOCK_OFF_TIME));
+    }
 
-  // After the suspense_time, the coffin will knock randomly for a total
-  // amount of time in this range.
-  long min_run_time;
-  long max_run_time;
+    // After a sequence, motion triggers are ignored for the lockout time.
+    long lockout_time() const {
+      return get(LOCKOUT_TIME);
+    }
 
-  // Each knock will engage the solenoid for a random time in this range.
-  long min_knock_time;
-  long max_knock_time;
+    bool load_defaults() {
+      m_version = current_version;
+      set(SUSPENSE_TIME,         0);
+      set(MIN_RUN_TIME,       2500);
+      set(MAX_RUN_TIME,       6000);
+      set(MIN_KNOCK_ON_TIME,   200);
+      set(MAX_KNOCK_ON_TIME,   400);
+      set(MIN_KNOCK_OFF_TIME,  200);
+      set(MAX_KNOCK_OFF_TIME,  900);
+      set(LOCKOUT_TIME,       6000);
+      return true;
+    }
 
-  // Amount of time between knocks.
-  long min_noknock_time;
-  long max_noknock_time;
+    bool load_from_eeprom(int base_address = 0) {
+      auto addr = base_address;
+      int signature = 0;
+      EEPROM.get(addr, signature); addr += sizeof(signature);
+      if (signature != magic) return false;  // never been saved
 
-  // After the sequence plays, we lock out re-triggering to discourage folks
-  // from lingering to see it again.
-  long lockout_time;
+      EEPROM.get(addr, m_version); addr += sizeof(m_version);
+      if (m_version <= 0) return false;
 
-  static int constexpr current_version = 1;
-  static int constexpr magic = 307;
+      auto *p = reinterpret_cast<uint8_t *>(m_values);
+      for (unsigned i = 0; i < sizeof(m_values); ++i) {
+        *p++ = EEPROM.read(addr++);
+      }
 
-  bool load_defaults() {
-    version = current_version;
-    suspense_time    =     0;
-    min_run_time     =  2500;
-    max_run_time     =  6000;
-    min_knock_time   =   200;
-    max_knock_time   =   400;
-    min_noknock_time =   200;
-    max_noknock_time =   900;
-    lockout_time     =  6000;
-    return true;
-  }
+      return m_version == current_version;
+    }
 
-  bool load_from_eeprom(int base_address = 0) {
-    int signature = 0;
-    EEPROM.get(base_address, signature);
-    if (signature != magic) return false;  // never been saved
-    auto address = base_address + sizeof(signature);
-    EEPROM.get(address, version); address += sizeof(version);
-    if (version == 0 || version > current_version) return false;
-    EEPROM.get(address, suspense_time); address += sizeof(suspense_time);
-    EEPROM.get(address, min_run_time); address += sizeof(min_run_time);
-    EEPROM.get(address, max_run_time); address += sizeof(max_run_time);
-    EEPROM.get(address, min_knock_time); address += sizeof(min_knock_time);
-    EEPROM.get(address, max_knock_time); address += sizeof(max_knock_time);
-    EEPROM.get(address, min_noknock_time); address += sizeof(min_noknock_time);
-    EEPROM.get(address, max_noknock_time); address += sizeof(max_noknock_time);
-    EEPROM.get(address, lockout_time); address += sizeof(lockout_time);
+    bool save_to_eeprom(int base_address = 0) const {
+      auto addr = base_address;
+      auto const signature = magic;
+      EEPROM.put(addr, signature); addr += sizeof(signature);
+      EEPROM.put(addr, m_version); addr += sizeof(m_version);
+      auto const *p = reinterpret_cast<uint8_t const *>(m_values);
+      for (unsigned i = 0; i < sizeof(m_values); ++i) {
+        EEPROM.update(addr++, *p++);
+      }
+      return true;
+    }
 
-    return version == current_version;
-  }
+    void clear_eeprom(int base_address = 0) {
+      // All we have to do is wipe out the magic number.
+      auto const signature = (magic == 0) ? 1 : 0;
+      EEPROM.put(base_address, signature);
+    }
 
-  bool save_to_eeprom(int base_address = 0) {
-    auto const signature = magic;
-    auto address = base_address + sizeof(signature);
-    EEPROM.put(address, version); address += sizeof(version);
-    EEPROM.put(address, suspense_time); address += sizeof(suspense_time);
-    EEPROM.put(address, min_run_time); address += sizeof(min_run_time);
-    EEPROM.put(address, max_run_time); address += sizeof(max_run_time);
-    EEPROM.put(address, min_knock_time); address += sizeof(min_knock_time);
-    EEPROM.put(address, max_knock_time); address += sizeof(max_knock_time);
-    EEPROM.put(address, min_noknock_time); address += sizeof(min_noknock_time);
-    EEPROM.put(address, max_noknock_time); address += sizeof(max_noknock_time);
-    EEPROM.put(address, lockout_time); address += sizeof(lockout_time);
-    EEPROM.put(base_address, signature);
-    return true;
-  }
+    bool sane() const {
+      unsigned long constexpr one_minute = 60 * 1000;
+      if (m_version != current_version) return false;
+      if (get(SUSPENSE_TIME) < 0 || one_minute < get(SUSPENSE_TIME)) return false;
+      if (get(MIN_RUN_TIME) < 1000) return false;
+      if (get(MAX_RUN_TIME) < get(MIN_RUN_TIME) || one_minute < get(MAX_RUN_TIME)) return false;
+      if (get(MIN_KNOCK_ON_TIME) < 200) return false;
+      if (get(MAX_KNOCK_ON_TIME) < get(MIN_KNOCK_ON_TIME) || one_minute < get(MAX_KNOCK_ON_TIME)) return false;
+      if (get(MIN_KNOCK_OFF_TIME) < 200) return false;
+      if (get(MAX_KNOCK_OFF_TIME) < get(MIN_KNOCK_OFF_TIME) || one_minute < get(MAX_KNOCK_OFF_TIME)) return false;
+      if (get(LOCKOUT_TIME) < 0 || one_minute < get(LOCKOUT_TIME)) return false;
+      return true;
+    }
 
-  bool sane() const {
-    if (version != current_version) return false;
-    if (suspense_time < 0 || 60000 < suspense_time) return false;
-    if (min_run_time < 1000) return false;
-    if (max_run_time < min_run_time || 60000 < max_run_time) return false;
-    if (min_knock_time < 200) return false;
-    if (max_knock_time < min_knock_time || 60000 < max_knock_time) return false;
-    if (min_noknock_time < 200) return false;
-    if (max_noknock_time < min_noknock_time || 60000 < max_noknock_time) return false;
-    if (lockout_time < 0 || 60000 < lockout_time) return false;
-    return true;
-  }
+    void show() const {
+      for (int i = 0; i < COUNT; ++i) {
+        Serial.print(F(" "));
+        Serial.print(i+1);
+        Serial.print(F(". "));
+        char buffer[32] = "";
+        load_name(i, buffer);
+        Serial.print(buffer);
+        Serial.print(F(" = "));
+        Serial.println(m_values[i]);
+      }
+    }
 
-  void show() const {
-    Serial.print(F(" Version:  ")); Serial.println(version);
-    Serial.print(F(" Suspense: ")); Serial.println(suspense_time);
-    Serial.print(F(" Run:      ")); Serial.print(min_run_time); Serial.print(F(" - ")); Serial.println(max_run_time);
-    Serial.print(F(" Knock:    ")); Serial.print(min_knock_time); Serial.print(F(" - ")); Serial.println(max_knock_time);
-    Serial.print(F(" No Knock: ")); Serial.print(min_noknock_time); Serial.print(F(" - ")); Serial.println(max_noknock_time);
-    Serial.print(F(" Lockout:  ")); Serial.println(lockout_time);
-  }
-} timing;
+  private:
+    enum Index {
+      // VERSION 1
+      SUSPENSE_TIME,
+      MIN_RUN_TIME,
+      MAX_RUN_TIME,
+      MIN_KNOCK_ON_TIME,
+      MAX_KNOCK_ON_TIME,
+      MIN_KNOCK_OFF_TIME,
+      MAX_KNOCK_OFF_TIME,
+      LOCKOUT_TIME,
+
+      // FUTURE VERSIONS ADD INDEXES HERE
+
+      // TOTAL NUMBER OF PARAMETERS
+      COUNT
+    };
+
+    long get(Index i) const {
+      return (0 <= i && i < COUNT) ? m_values[i] : 0L;
+    }
+
+    bool set(Index i, long value) {
+      if (0 <= i && i < COUNT) {
+        m_values[i] = value;
+        return true;
+      }
+      return false;
+    }
+
+    static void load_name(Index i, char buffer[]) {
+      if (0 <= i && i < COUNT) {
+        auto const source =
+          reinterpret_cast<char const *>(pgm_read_word(m_names + i));
+        strcpy_P(buffer, source);
+      } else {
+        strcpy(buffer, "OOB");
+      }
+    }
+
+    int m_version;
+    long m_values[COUNT];
+
+    static char const * const m_names[COUNT];
+    static int constexpr current_version = 1;
+    static int constexpr magic = 307;
+} params;
+
+char const NAME_Suspense[] PROGMEM = "Suspense";
+char const NAME_Min_Run[]  PROGMEM = "Min Run";
+char const NAME_Max_Run[]  PROGMEM = "Max Run";
+char const NAME_Min_Knock_On[] PROGMEM = "Min Knock On";
+char const NAME_Max_Knock_On[] PROGMEM = "Max Knock On";
+char const NAME_Min_Knock_Off[] PROGMEM = "Min Knock Off";
+char const NAME_Max_Knock_Off[] PROGMEM = "Max Knock Off";
+char const NAME_Lockout[] PROGMEM = "Lockout";
+char const * const Parameters::m_names[Parameters::COUNT] PROGMEM = {
+  NAME_Suspense,
+  NAME_Min_Run, NAME_Max_Run,
+  NAME_Min_Knock_On, NAME_Max_Knock_On,
+  NAME_Min_Knock_Off, NAME_Max_Knock_Off,
+  NAME_Lockout
+};
 
 enum class State {
   initializing,
@@ -157,26 +223,25 @@ enum class State {
 
 // timeout is the time the next state change should occur (millis).
 unsigned long timeout = 0;
-
+// knock_timeout is the time the current knock_on or knock_off ends.
 unsigned long knock_timeout = 0;
 
 static long eat_input() {
   while (Serial.available()) Serial.read();
 }
 
-static long pick_time(long low, long high) {
-  return random(low, high);
-}
-
 void setup() {
-  Serial.begin(115200);
   pinMode(solenoid_pin, OUTPUT);
   digitalWrite(solenoid_pin, LOW);
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
   pinMode(trigger_pin, INPUT);
+  Serial.begin(115200);
   while (!Serial) {}  // not necessary for Pro Mini, but no harm
   Serial.println(F("\nCoffin Knocker"));
+
+  EEPROM.begin();
+
   int const seed = analogRead(A0);
   Serial.print(F("Random Seed: "));
   Serial.println(seed);
@@ -205,17 +270,17 @@ void loop() {
 
   switch (state) {
     case State::initializing: {
-      if (timing.load_from_eeprom(0) && timing.sane()) {
+      if (params.load_from_eeprom(0) && params.sane()) {
         Serial.println(F("Timing parameters loaded from EEPROM."));
-        timing.show();
+        params.show();
         state = State::waiting;
         Serial.print(now);
         Serial.println(F(" Waiting..."));
         break;
       }
-      if (timing.load_defaults() && timing.sane()) {
+      if (params.load_defaults() && params.sane()) {
         Serial.println(F("Using default timing parameters."));
-        timing.show();
+        params.show();
         state = State::waiting;
         Serial.print(now);
         Serial.println(F(" Waiting..."));
@@ -228,7 +293,7 @@ void loop() {
 
     case State::waiting: {
       if (trigger == HIGH) {
-        timeout = now + timing.suspense_time;
+        timeout = now + params.suspense_time();
         state = State::suspense;
         Serial.print(now);
         Serial.println(F(" Triggered"));
@@ -245,7 +310,7 @@ void loop() {
           Serial.println(F(" Canceled"));
           state = State::waiting;
         } else {
-          long const run_time = pick_time(timing.min_run_time, timing.max_run_time);
+          long const run_time = params.random_run_time();
           timeout = now + run_time;
           knock_timeout = now;
           state = State::knock_off;
@@ -258,15 +323,15 @@ void loop() {
 
     case State::knock_on: {
       if (now >= timeout) {
-        timeout = now + timing.lockout_time;
+        timeout = now + params.lockout_time();
         state = State::lockout;
         Serial.print(now);
         Serial.println(F(" Lockout"));
         break;
       }
       if (now >= knock_timeout) {
-        long const noknock_time = pick_time(timing.min_noknock_time, timing.max_noknock_time);
-        knock_timeout = now + noknock_time;
+        long const knock_off_time = params.random_knock_off_time();
+        knock_timeout = now + knock_off_time;
         state = State::knock_off;
       }
       break;
@@ -274,14 +339,14 @@ void loop() {
 
     case State::knock_off: {
       if (now >= timeout) {
-        timeout = now + timing.lockout_time;
+        timeout = now + params.lockout_time();
         state = State::lockout;
         Serial.print(now);
         Serial.println(F(" Lockout"));
         break;
       }
       if (now >= knock_timeout) {
-        long const knock_time = pick_time(timing.min_knock_time, timing.max_knock_time);
+        long const knock_time = params.random_knock_on_time();
         knock_timeout = now + knock_time;
         state = State::knock_on;
         Serial.print(now);
@@ -307,26 +372,30 @@ void loop() {
       if (' ' < ch && ch < 127) Serial.println((char) ch); else Serial.println(ch);
 
       switch (ch) {
+        case 'C':
+          params.clear_eeprom();
+          break;
         case 'D':
-          timing.load_defaults();
+          params.load_defaults();
           [[fallthrough]]
         case 'L':
-          timing.show();
+          params.show();
           break;
         case 'S':
-          if (timing.sane() && timing.save_to_eeprom()) {
+          if (params.sane() && params.save_to_eeprom()) {
             state = State::waiting;
           } else {
             Serial.println(F("Could not save settings to EEPROM."));
           }
           break;
         case 'R':
-          state = timing.sane() ? State::waiting : State::initializing;
+          state = params.sane() ? State::waiting : State::initializing;
           break;
       }
       eat_input();
       if (state == State::programming) {
-        Serial.print(F("(L)ist, restore (D)efaults, (S)ave, (R)un: "));
+        Serial.print(F("(L)ist, (D)efaults, (S)ave, (C)lear, (R)un: "));
+        Serial.flush();
       }
       if (state == State::waiting) {
         Serial.println(F("Waiting..."));
